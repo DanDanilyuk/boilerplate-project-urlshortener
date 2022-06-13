fdce8fcff88d79244b7c8f78aa4c5ea24af9c88e
Dan Danilyuk <dandanilyuk@gmail.com>
2022-06-13 10:26:35 -0600

    Add mongodb and mongoose to app

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ require('dotenv').config();
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const express = require('express');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const mySecret = process.env['MONGO_URI'];
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true })); 
+mongoose.connect(mySecret, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
